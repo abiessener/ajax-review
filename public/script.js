c3f1e682341a2b34c7031772e04a53660c35e710
Adam Biessener <abiessener@gmail.com>
2017-08-03 11:48:32 -0500

    added input field and button for updating the greeting on the server

--- public/script.js
+++ public/script.js
@@ -1,17 +1,55 @@
-function appendHello(){
-        $.ajax('/hello', {
-            method: 'GET',
-            success: function(response){
-                $('body').append('<div>' + response + '</div>');
+function appendHello() {
+    $.ajax('/hello', {
+        method: 'GET',
+        success: function (response) {
+            $('body').append('<div>' + response + '</div>');
+        }
+    });
+}
+
+function getJacked(){
+    $.ajax('/pumpup', {
+        method: 'POST',
+        data: '!',
+        success: function(response){
+            console.log('getJacked success');
+            
+        }
+    })
+}
+
+function updateGreeting(){
+    var updatedGreeting = $('#greetingInput').val();
+    $('#greetingInput').val('');
+    $.ajax('/hello', {
+        method: 'POST',
+        data: { newGreeting: updatedGreeting },
+        success: function(response){
+            console.log('updateGreeting success:', updatedGreeting);
+            appendHello();
+        },
+        statusCode: {
+            400: function(){
+                window.alert('no ghost greetings, please!');
             }
-        });
+        }
+    });
 }
 
 
-$(document).ready(function(){
-    $('h1').css('font-size', '9em');
+$(document).ready(function () {
+    $('h1').css('font-size', '2em');
 
-    $('#helloButton').on('click',appendHello);
+    $('#helloButton').on('click', appendHello);
+
+    $('#pumpUpButton').on('click', getJacked);
+
+    $('#greetingSendButton').on('click', updateGreeting);
+
+    $('#greetingInput').keypress(function(key){
+        if (key.which == 13){
+            updateGreeting();
+        }
+    });
 
 });
-
--- server/app.js
+++ server/app.js
@@ -4,11 +4,33 @@ var app = express();
 
 var port = 5000;
 
+var greeting = 'hello world';
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 app.get('/hello', function(req,res){
-    res.send('hello world');
+    res.send(greeting);
+});
+
+app.post('/hello', function(req,res){
+    var newGreeting = req.body.newGreeting;
+    // console.log('data type:', typeof(req.body.newGreeting));
+    // console.log('newGreeting:', req.body.newGreeting);
+    
+    if (newGreeting === ''){
+        res.sendStatus(400);
+    } else if (typeof(newGreeting) != 'string') {
+        res.sendStatus(401);
+    } else {
+        greeting = newGreeting;
+        res.sendStatus(201);
+    }
+});
+
+app.post('/pumpup', function(req,res){
+    greeting += '!';
+    res.sendStatus(201);
 });
 
 app.listen(port, function(){
