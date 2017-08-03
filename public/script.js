a47e78831c00bce4311bc68ca4b786f5e7d6035d
Adam Biessener <abiessener@gmail.com>
2017-08-03 09:48:33 -0500

    initial commit

---
+++ public/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('h1').css('font-size', '9em');
+});
+
---
+++ server/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var port = 5000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.listen(port, function(){
+    console.log('listening on port', port);
+    
+})
