684f2089be1592c3e4806dcb0cf24a3bbb971f48
Adam Biessener <abiessener@gmail.com>
2017-08-03 10:46:35 -0500

    moved ajax call into the new appendHello function

--- public/script.js
+++ public/script.js
@@ -1,14 +1,17 @@
-$(document).ready(function(){
-    $('h1').css('font-size', '9em');
-
-    $('#helloButton').on('click',function(){
+function appendHello(){
         $.ajax('/hello', {
             method: 'GET',
             success: function(response){
                 $('body').append('<div>' + response + '</div>');
             }
         });
-    })
+}
+
+
+$(document).ready(function(){
+    $('h1').css('font-size', '9em');
+
+    $('#helloButton').on('click',appendHello);
 
 });
 
