b3586908999dc233a27539c422f1c1d9e1e3dfcd
Adam Biessener <abiessener@gmail.com>
2017-08-03 10:20:28 -0500

    added hello world handler to /hello

--- server/app.js
+++ server/app.js
@@ -7,7 +7,11 @@ var port = 5000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
+app.get('/hello', function(req,res){
+    res.send('hello world');
+});
+
 app.listen(port, function(){
     console.log('listening on port', port);
     
-})
+});
