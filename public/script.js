15b7cecbeb67de0f98c51486cc4e1d4122080c20
Adam Biessener <abiessener@gmail.com>
2017-08-03 10:27:47 -0500

    added hello world button

--- public/script.js
+++ public/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
     $('h1').css('font-size', '9em');
+
+    $('#helloButton').on('click',function(){
+        $.ajax('/hello', {
+            method: 'GET',
+            success: function(response){
+                $('body').append(response);
+            }
+        });
+    })
+
 });
 
